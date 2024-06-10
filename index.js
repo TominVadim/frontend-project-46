b576c6b6abeaaff4139d87b8c9270a39e59a0e4e
TominVadim <tomin.1991@inbox.ru>
2024-06-10 22:03:47 +0300

    4 step

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import path from 'path';
 import fs from 'fs';
+import _ from 'lodash';
+const readfile = (filepath) => {
+	const thePath = path.resolve(process.cwd(), filepath);
+	return  JSON.parse(fs.readFileSync(thePath, 'utf-8'));
+};
 
 const gendiff = (filepath1, filepath2) => {
-	const [path1, path2] = [path.resolve(process.cwd(), filepath1),
-	path.resolve(process.cwd(), filepath2)];
-	const data1 = JSON.parse(fs.readFileSync(path1, 'utf-8'));
-	const data2 = JSON.parse(fs.readFileSync(path2, 'utf-8'));
-	return [data1, data2];
-}
+	const data1 = readfile(filepath1);
+	const data2 = readfile(filepath2);
+
+	const keys = _.union(_.keys(data1), _.keys(data2)).sort();
+	const diffObj = keys.map((key)=>{
+		if (_.has(data1, key) && !_.has(data2, key)) {
+			return `  - ${key}: ${data1[key]}`;
+		}
+		if (_.has(data2, key) && !_.has(data1, key)) {
+			return `  + ${key}: ${data2[key]}`
+		}
+
+		if (data1[key] !== data2[key]){
+			return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`
+		}
+		return `    ${key}: ${data1[key]}`
+	})
+	return `{\n${diffObj.join('\n')}\n}`;
+};
 
 export default gendiff;
